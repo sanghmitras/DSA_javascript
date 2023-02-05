696ccf1aeb6d6f8c812a546ab3c14b2d6a497848
Sanghmitra Singh Rathore <sanghmitra1993@gmail.com>
2023-02-05 15:20:43 +0530

    insert node

--- linkedList.js
+++ linkedList.js
@@ -1,46 +1,63 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
 
-class Node{
-constructor(value){
-	this.value = value
-  this.next = null
-}
-}
- class LinkedList{
-  constructor(){
-  this.head = null;
-  this.size = 0
-  }
-  
-  add(element){
-    var newNode = new Node(element)
-    var tempHead = this.head
-    if(this.head == null){
-        this.head = newNode
-    }	else{
-      while(tempHead.next){
-        tempHead = tempHead.next
+  add(element) {
+    var newNode = new Node(element);
+    var tempHead = this.head;
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      while (tempHead.next) {
+        tempHead = tempHead.next;
       }
-      tempHead.next = newNode
+      tempHead.next = newNode;
     }
-    this.size ++
+    this.size++;
   }
-  getNode(index){
-  console.log('size', this.size, index)
-  	if(this.size <= index || this.size==0){
-		return `incorrect index, max size ${this.size}`
-    }else{
-    var tempHead = this.head
-    	let nodeNumber = 0
-			while(nodeNumber < index){
-				tempHead = tempHead.next
-        nodeNumber ++;
+  getNode(index) {
+    if (this.size <= index || this.size == 0) {
+      return `incorrect index, max size ${this.size}`;
+    } else {
+      var tempHead = this.head;
+      let nodeNumber = 0;
+      while (nodeNumber < index) {
+        tempHead = tempHead.next;
+        nodeNumber++;
       }
-     return tempHead
-      }
+      return tempHead;
+    }
   }
-} 
-let l =new LinkedList()
-l.add('Hi')
-l.add("name")
-l.add('onemore')
-console.log(l.getNode(0))
+  insertNode(value, index) {
+    if (this.size <= index || this.size == 0 || index == 0) {
+      console.log(`invalid index ${index}`);
+      return false;
+    } else {
+      let newNode = new Node(value);
+      var tempHead = this.head;
+      let current = 0;
+      var prevHead;
+      while (current < index) {
+        prevHead = tempHead;
+        tempHead = tempHead.next;
+        current++;
+      }
+      newNode.next = tempHead;
+      prevHead.next = newNode;
+    }
+  }
+}
+let l = new LinkedList();
+l.add("first Node");
+l.add("Second Node");
+l.add("third Node");
+l.insertNode("second again", 0);
+console.log(l.getNode(0));
